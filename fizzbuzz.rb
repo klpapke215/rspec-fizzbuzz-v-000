ff0569a54432fb0196fe48a910923ee3d97d03db
Kristen Papke <klpapke215@gmail.com>
2018-05-31 15:53:46 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,13 +1,10 @@
 def fizzbuzz(int)
   if int % 3 == 0
     "Fizz"
-  end
   elsif int % 5 == 0
   "Buzz"
-  end
   elsif int % 3 == 0 && int % 5 == 0
   "Fizzbuzz"
-  end
   else int % 3 !== 0 && int % 5 !== 0
   "Nil"
   end
