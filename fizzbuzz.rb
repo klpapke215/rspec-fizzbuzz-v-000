b2ff8dd4c50f7e5584c9793785f9a6bac233a1a6
Kristen Papke <klpapke215@gmail.com>
2018-05-31 16:13:30 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,11 +1,11 @@
 def fizzbuzz(int)
-  if int % 3 == 0 && int % 5 !== 0
-    puts "Fizz"
+  if int % 3 == 0 && int % 5 == 0
+    puts "Fizzbuzz"
   elsif int % 5 == 0 && int % 3 !== 0
     puts "Buzz"
-  elsif int % 3 == 0 && int % 5 == 0
-    puts "Fizzbuzz"
-  else 
+  elsif int % 3 == 0 && int % 5 !== 0
+  puts "Fizz"
+  else
     puts "Nil"
   end
 end
